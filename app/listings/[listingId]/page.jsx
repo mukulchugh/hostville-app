81ebc486ab8ebb4aab569b13c2a3e6ffbf1ebe49
zenmukul <mukul@zenduty.com>
2023-06-06 00:01:45 +0530

    wdsfds

--- app/listings/[listingId]/page.jsx
+++ app/listings/[listingId]/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import getReservations from "@/app/actions/getReservations";
--- pages/admin/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AdminLayout from "./layout";
-
-const page = () => {
-  return <AdminLayout>page</AdminLayout>;
-};
-
-export default page;
