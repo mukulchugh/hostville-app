c8fdcc750c1d5d9f82800484dadd6a0a47ced734
zenmukul <mukul@zenduty.com>
2023-06-06 00:03:23 +0530

    df

--- app/listings/[listingId]/page.jsx
+++ app/listings/[listingId]/page.jsx
@@ -1,4 +1,3 @@
-"use client";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import getReservations from "@/app/actions/getReservations";
